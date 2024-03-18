d63976afba9dca5781b11927c3f57683fe2574e6
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-18 21:34:26 +0300

    feat: add LeadStatus component

---
+++ src/components/LeadStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-tag :color="color">{{ status }}</a-tag>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{ status: string }>()
+const color = computed(() => {
+  if (/успешн/i.test(props.status)) {
+    return 'green'
+  }
+
+  if (/не реализ/.test(props.status)) {
+    return 'volcano'
+  }
+
+  if (/Согласование договора/.test(props.status)) {
+    return 'yellow'
+  }
+
+  return 'blue'
+})
+</script>
+
+<style scoped></style>
