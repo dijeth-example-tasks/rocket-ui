40eff97959ed409ea17c017e5f313d7fee5087dc
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-18 21:02:39 +0300

    remove unusable plugin

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,4 +1,3 @@
-import { useProgress } from '../plugins/progress'
 import { ref, type Ref } from 'vue'
 import type { ApiResponse } from '@/api/api'
 
@@ -9,51 +8,22 @@ export type TApiReturn<T> = {
   error: Ref<any>
   request: TApiFunction<T>
 }
-export const API_NATIVE_RESPONSE = 'API_NATIVE_RESPONSE'
 
-/**
- * Компосабл для выполнения запросов в компонентах
- * Добавляет опциональную интеграцию с прогресс баром
- * Возвращает реактивные переменные с результатом
- */
-export function useApi<T>(
-  apiFunction: TApiFunction<T>,
-  { showProgress = false, progressKey = '' } = {},
-): TApiReturn<T> {
+export function useApi<T>(apiFunction: TApiFunction<T>): TApiReturn<T> {
   const result = ref<T>()
   const error = ref<any>()
   const isLoading = ref<boolean>(false)
-  const progress = useProgress()
-  const progressId = progressKey || Date.now().toString()
-
-  const startProgress = () => {
-    if (progress && showProgress) {
-      progress.start(progressId)
-    }
-  }
-  const finishProgress = () => {
-    if (progress && showProgress) {
-      progress.finish(progressId)
-    }
-  }
-  const failProgress = () => {
-    if (progress && showProgress) {
-      progress.fail()
-    }
-  }
 
   const request = async (...args: any[]) => {
     isLoading.value = true
     result.value = undefined
     error.value = undefined
-    startProgress()
+
     const response = await apiFunction(...args)
     if (response.error) {
       error.value = response.error
-      failProgress()
     } else {
       result.value = response.data
-      finishProgress()
     }
     isLoading.value = false
 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,5 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-import { createProgress } from './plugins/progress'
 
-createApp(App).use(router).use(createProgress()).mount('#app')
+createApp(App).use(router).mount('#app')
