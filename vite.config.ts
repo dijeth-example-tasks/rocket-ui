0868c6cd0771453e97ce08397589b8e26afeb593
Dmitry Orlov <d.orlov777@gmail.com>
2024-03-19 12:58:49 +0300

    fix: github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,16 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '' : '/rocket-ui/',
   plugins: [
     vue(),
     Components({
-      resolvers: [AntDesignVueResolver({ importStyle: false })]
-    })
+      resolvers: [AntDesignVueResolver({ importStyle: false })],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
